5ef8333078d1d3969c37ef8e07551e4dc755fac3
Christopher Polk <93009240+Jamal85140@users.noreply.github.com>
2022-03-29 20:21:04 -0700

    renamed layout

--- app/src/main/java/com/fromthesky/healthyshares/SettingsActivity.java
+++ app/src/main/java/com/fromthesky/healthyshares/SettingsActivity.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Build;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
     }
     public String getAndroidVersion() {
         String release = Build.VERSION.RELEASE;
