0118f153afc24a6dcf59a473e6e4eb15161eb566
Christopher Polk <93009240+Jamal85140@users.noreply.github.com>
2022-03-29 20:06:55 -0700

    complete layout

--- app/src/main/java/com/fromthesky/healthyshares/MainActivity.java
+++ app/src/main/java/com/fromthesky/healthyshares/MainActivity.java
@@ -2,6 +2,7 @@ package com.fromthesky.healthyshares;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public String getAndroidVersion() {
+        String release = Build.VERSION.RELEASE;
+        int sdkVersion = Build.VERSION.SDK_INT;
+        return "Android SDK: " + sdkVersion + " (" + release +")";
     }
 }
