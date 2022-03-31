35fc13c9fe5fc31776b051823b3bd4c15455d3da
Christopher Polk <93009240+Jamal85140@users.noreply.github.com>
2022-03-31 09:27:41 -0700

    daily
    daily push

--- app/src/main/java/com/fromthesky/healthyshares/SettingsActivity.java
+++ app/src/main/java/com/fromthesky/healthyshares/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.fromthesky.healthyshares;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -11,10 +12,26 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
     }
+/*
+    protected void onItemClick() {
+        //please update MainActivity.class as needed
+        Intent intent = new Intent();
+        intent.setClass(SettingsActivity.this, MainActivity.class);
+        startActivity(intent);
+
+    }
+*/
+
     public String getAndroidVersion() {
         String release = Build.VERSION.RELEASE;
         int sdkVersion = Build.VERSION.SDK_INT;
         return "Android SDK: " + sdkVersion + " (" + release +")";
+
+// make add button add to list
+        //intents for home page
+        //arraylist to fill listView
     }
 }
